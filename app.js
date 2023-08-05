9a9da6b073ed6f1948ce7173cbbf383648f314c5
Anna_Hubskaya <anygubskaya@outlook.com>
2023-08-05 21:14:40 +0300

    add env

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const analyticsRoutes = require("./routes/analytics");
 const categoryRoutes = require("./routes/category");
 const orderRoutes = require("./routes/order");
 const positionRoutes = require("./routes/position");
-const keys = require("./config/keys");
 const app = express();
+const functions = require("firebase-functions"); 
 
 mongoose
   .connect(keys.mongoURL, { dbName: keys.dbName }, {useNewUrlParser: true})
@@ -34,4 +34,13 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/position", positionRoutes);
 
-module.exports = app;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
+const api = functions.https.onRequest(app)
+module.exports = api;
