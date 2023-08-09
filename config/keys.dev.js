97b0978541cf0d1af07fde6560d72ba948d80068
Anna_Hubskaya <anygubskaya@outlook.com>
2023-08-09 11:32:23 +0300

    remove files

--- config/keys.dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURL: `mongodb+srv://Anya:anyaGubskaya1994@cluster0.v6djjpi.mongodb.net/?retryWrites=true&w=majority`,
-  dbName: "crm",
-  jwt: "dev-jwt",
-  apiUrl: "https://smart-assistant.onrender.com"
-};
