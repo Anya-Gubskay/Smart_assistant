3fd811d8db29539c0805705304d48485a34d14ca
Anna_Hubskaya <anygubskaya@outlook.com>
2023-08-09 14:48:37 +0300

    changed path

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, '../../../../../uploads1');
+    cb(null, '../../../../../uploads');
   },
 
   filename(rea, file, cb) {
