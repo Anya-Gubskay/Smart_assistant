02b2574b0e52405b5ce6db83f6885063a2ee475c
Anna_Hubskaya <anygubskaya@outlook.com>
2023-08-09 13:39:13 +0300

    remove-top progress-bar index

--- client/src/app/shared/components/top-progress-bar/top-progress-bar/top-progress-bar.module.ts
+++ client/src/app/shared/components/top-progress-bar/top-progress-bar/top-progress-bar.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
 import { TopProgressBarContainer } from './top-progress-bar.container';
-import { TopProgressBarStoreFeatureModule } from 'src/app/store/top-progress-bar';
 @NgModule({
 	declarations: [TopProgressBarContainer],
 	exports: [TopProgressBarContainer],
-	imports: [TopProgressBarStoreFeatureModule],
 })
 export class TopProgressBarModule {
 
